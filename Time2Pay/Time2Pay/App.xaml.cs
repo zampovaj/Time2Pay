d9b3268b84c4db38ac7d4067bb6f1831bf1f3770
Johana Žampová <155270869+zampovaj@users.noreply.github.com>
2024-12-23 14:42:50 +0100

    Time2Pay
    Project upload

---
+++ Time2Pay/Time2Pay/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Time2Pay
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
