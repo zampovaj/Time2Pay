2c3c0be640a07fc95cd89309fc36c80a866c9c1b
Johana Žampová <zampovaj.06@spst.eu>
2025-01-03 21:38:40 +0100

    gas model

---
+++ Time2Pay/Time2Pay/Model/Gas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time2Pay.Model
+{
+    internal class Gas
+    {
+        private int id;
+        private int salaryId;
+        private double cost;
+        public DateTime Date { get; private set; }
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Id cannot be negative.");
+                id = value;
+            }
+        }
+        public int SalaryId
+        {
+            get => id;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Id cannot be negative.");
+                id = value;
+            }
+        }
+        public double Cost
+        {
+            get => cost;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Cost cannot be negative");
+                cost = value;
+            }
+    }
+}
